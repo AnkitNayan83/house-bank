468db3ecb6f277c4fb4d1f39ec34bffd30ca9c36
AnkitNayan83 <ankitnayan83@gmail.com>
2025-05-09 06:17:02 +0000

    Add database migration support and update Dockerfile dependencies
    - Integrated `golang-migrate/migrate` for handling database migrations.
    - Updated Dockerfile to use the latest Go and Alpine versions.
    - Modified `start.sh` to remove direct migration commands.
    - Enhanced `Config` struct to include `MigrationURL` for migration configuration.

--- util/config.go
+++ util/config.go
@@ -8,6 +8,7 @@ import (
 
 type Config struct {
 	DBSource               string        `mapstructure:"DB_SOURCE"`
+	MigrationURL           string        `mapstructure:"MIGRATION_URL"`
 	HttpServerAddress      string        `mapstructure:"HTTP_SERVER_ADDRESS"`
 	GRPCServerAddress      string        `mapstructure:"GRPC_SERVER_ADDRESS"`
 	TOKEN_SYMMETRIC_KEY    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
